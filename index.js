b2bf7a97001cb2781459c0520e93f911db3fbce4
root <rick@rudelinux.org>
2020-05-30 21:44:53 +0100

    just a little refactor

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const https = require('https');
-const convert = require('xml-js');
-const os = require('os');
-const fs = require('fs');
-const { spawn } = require('child_process')
-const readline = require('readline');
+// const convert = require('xml-js');
+// const os = require('os');
+// const fs = require('fs');
+// const readline = require('readline');
 
 const app = express();
 enableWs(app)
@@ -17,12 +16,10 @@ enableWs(app)
 // Local libs
 const getPort = require('./lib/getPort');
 const port = getPort();
-const Config = require('./lib/config')
-const MetarRequest = require('./lib/metar_request').MetarRequest
-const TafRequest = require('./lib/metar_request').TafRequest
+// const MetarRequest = require('./lib/metar_request').MetarRequest
+// const TafRequest = require('./lib/metar_request').TafRequest
 const WeatherRequest = require('./lib/metar_request').WeatherRequest
 const MapLightControllerFactory = require('./lib/map_light_controller_factory');
-const Cache = require('./lib/cache');
 const mapLightController = MapLightControllerFactory.create()
 
 const BLEPeripheral = require('./lib/bluetooth/BLEPeripheral');
@@ -31,135 +28,25 @@ const logger = require('./lib/logger')('server');
 
 const Message = require('./lib/message');
 const messageTypes = require('./lib/message_types');
+const WebSocket = require('./lib/web_socket')
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('hello!')
-})
-
 app.ws('/metar.ws', (ws, req) => {
+  let websocket = new WebSocket(ws)
+
   ws.on('message', (msg) => {
-    const message = new Message(msg)
-    switch(message.type){
-      case messageTypes.SUBSCRIBE:
-        switch(message.payload){
-          case messageTypes.subscribe.METARS:
-            logger.info("metars RX");
-            // This might have broken the ios app
-            //sendData(ws, 'metar-data', WeatherRequest.json())
-            sendData(ws, 'metars', WeatherRequest.json())
-            break;
-          case messageTypes.subscribe.AIRPORTS_AND_CATEGORIES:
-            sendData(ws, 'airports-and-categories', WeatherRequest.airportsAndCategories());
-            break;
-          case messageTypes.subscribe.LOGS:
-            logger.info("log message RX");
-            sendLogData(ws);
-            break;
-        }
-        break;
-      case messageTypes.LEDS:
-        switch(message.payload){
-          case messageTypes.leds.ON:
-            logger.info("ledState on");
-            mapLightController.lightsOn();
-            break;
-          case messageTypes.leds.OFF:
-            logger.info("ledState off");
-            mapLightController.lightsOff();
-            break;
-          case messageTypes.leds.MODE.RAINBOW:
-            logger.info("Changing to rainbow mode");
-            mapLightController.setMode(messageTypes.leds.MODE.RAINBOW);
-            break;
-          case messageTypes.leds.MODE.METAR:
-            logger.info("Changing to metar mode");
-            mapLightController.setMode(messageTypes.leds.MODE.METAR);
-            break;
-          case messageTypes.leds.STATUS:
-            logger.info('Sending', mapLightController.getLightStatus());
-            sendData(ws, 'light-status', mapLightController.getLightStatus(), false)
-            break;
-          default:
-            logger.info("Unknown leds message: " + msg);
-            break;
-        }
-        break;
-      case messageTypes.DATA:
-        switch(message.payload){
-          case messageTypes.data.UPDATE:
-            logger.info("Updating data");
-            WeatherRequest.update();
-            sendData(ws, 'metar-data', WeatherRequest.json(), false)
-            break;
-          default:
-            logger.info("Unknown data message: " + msg);
-            break;
-        }
-      default:
-        logger.info("RX unknown message type'" + msg + "'");
-        break;
-    }
+    logger.info("Got message " + msg);
+
+    const message = Message.create(msg, websocket, mapLightController, WeatherRequest)
+    message ? message.call() : false;
   })
 })
 
 // Serve our production build
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Sends log data to the ws client
-sendLogData = (ws) => {
-  let logLines = new Cache(100)
-
-  logger.info("Sending log data");
-  const latestLines = spawn('tail', ['-100', Config.log_file]);
-  latestLines.stdout.on('data', (line) => { logLines.store(line.toString()) })
-
-  const tail = spawn('tail', ['-F', Config.log_file]);
-
-  if(!ws){ logger.info("WS is null"); return false }
-  if(ws.readyState === 1){
-    tail.stdout.on('data', (line) => {
-      logLines.store(line.toString());
-      if(ws.readyState === 1){
-        ws.send(JSON.stringify({
-          type: "logs",
-          payload: logLines
-        }));
-      }
-    })
-  }
-}
-
-// Send data to the websocket client.
-// * ws           - the websocket
-// * payloadName  - populates the 'type' field of the message
-// * payload      - the data to be sent
-// * repeat       - if the data should be sent repeatedly
-// * sendInterval - how often to send the data
-sendData = (ws, payloadName, payload, repeat=true, sendInterval=10) => {
-  logger.debug('Sending ' + payloadName + ' data');
-  if(!ws){ logger.info("WS is null"); return false }
-
-  if(ws.readyState === 1){
-    if(payload.has_errors){
-      ws.send(JSON.stringify({
-        type: 'error',
-        payload: payload
-      }));
-    } else {
-      ws.send(JSON.stringify({
-        type: payloadName,
-        payload: payload,
-      }));
-    }
-
-    if(repeat) { setTimeout(sendData, sendInterval * 1000, ws, payloadName, payload, repeat) };
-  }else{
-    logger.info("Unknown ready state: " + ws.readyState);
-  }
-}
 
 // Begin fetching metars
 WeatherRequest.call();
